6eb01f6b0fa19de01e56c85578d2321d8f049742
Matheus <158523387+Teteusfc@users.noreply.github.com>
2024-07-03 21:40:30 -0300

    Add files via upload

---
+++ condicionais.py
@@ -0,0 +1,28 @@
+print(' ')
+
+#idade = 16
+#variavel de condição de idade
+#if idade == 16:
+# condição da variavel é 16
+   # print('menor de idade')
+# exibindo a informação menor de idade
+
+
+#year = int(input('qual a sua idade?\n')
+#if year < 16:
+  #  print('voce é menor de idade')
+#else: #  print('voce é maior de idade')    
+
+
+
+
+fetaria = int(input('Nos diga a sua idade:'))
+
+if fetaria > 18:
+   print('Que tal um filme de terror')
+
+elif fetaria >= 14:
+ print("Que tal um filme de ação")
+
+else: 
+ print("Que tal um filme Infantil") 
---
+++ lacosderepeticao.py
@@ -0,0 +1,19 @@
+
+# x = 0
+#while x < 5:
+ #nome = input ('Qual seu nome?' )
+ #x=x+1
+
+
+
+
+x = 0
+animais = [ ]
+  
+while x <5 :
+    
+    animaism= input ('cite 5 animais com a letra m')
+
+    animais.append(animais)
+    x=x+1
+    print(animais)
